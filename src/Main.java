367cf2b68085f9f888d4e803581f9f0d6d1fba5a
Aleksandr Osipov <eltisprim@gmail.com>
2020-11-06 20:54:40 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost_ticket = 169999;
+        int bonus_miles = 20;
+        int bonus = cost_ticket / bonus_miles;
+        System.out.println(bonus);
+    }
+}
+
