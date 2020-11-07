f94f57f5088899ef70f7d8e1718e7c62aae2bd59
Aleksandr Osipov <eltisprim@gmail.com>
2020-11-07 12:59:04 +0300

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Стоимост costTicket в копейках
+        // Стоимость билета costTicket в копейках
         long costTicket = 18_695_400;
         long bonus = costTicket / 20 / 100;
         System.out.println(bonus);
